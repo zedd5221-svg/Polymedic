4faf2f1a6b1ebe601085b9187d70a80e0d1b0a6d
Sarcon Ulama <sarconulama@yahoo.com>
2026-07-21 01:48:43 +0800

    added some logo though it doesnt work still

--- app/Views/Layouts/AppointmentLayout.php
+++ app/Views/Layouts/AppointmentLayout.php
@@ -10,7 +10,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </head>
 <body>
-    <?php echo $this->renderSection('AppointmentContent'); ?>
+    <?php echo $this->renderSection('AppointmentContent');  ?>
 
 
 
